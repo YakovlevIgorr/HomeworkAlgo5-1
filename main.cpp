fcba705d5139b1c0af0788049a9e902ad7d3b8d2
Igor <jaknikig@mail.ru>
2023-11-08 19:59:30 +0100

    change

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 int piramide_left(int* arr, int size, int number, int count){
 
     if((number * 2) + 1 >= size) return arr[number];
-    std::cout << count << " left(" <<  arr[number] << ") " << arr[(number * 2) + 1] << "\n";
+    std::cout << ceil(log2(count + 1)) << " left(" <<  arr[number] << ") " << arr[(number * 2) + 1] << "\n";
 
     if((number * 2) + 2 >= size) return arr[number];
-    std::cout << count << " right(" <<  arr[number] << ") " << arr[(number * 2) + 2] << "\n";
+    std::cout << ceil(log2(count + 1)) << " right(" <<  arr[number] << ") " << arr[(number * 2) + 2] << "\n";
 
     return piramide_left(arr, size, number + 1, count + 1);
 }
 
-void print_pyramid(int* array, int size){//1 3 6 5 9 8 //5
+void print_pyramid(int* array, int size){
 
     std::cout << "0 root " << array[0] << "\n";
 
@@ -23,7 +23,8 @@ void print_pyramid(int* array, int size){//1 3 6 5 9 8 //5
 int main() {
 
     //int array[] = {1, 3, 6, 5, 9, 8};
-    int array[] = {94, 67, 18, 44, 55, 12, 6, 42};
+    //int array[] = {94, 67, 18, 44, 55, 12, 6, 42};
+    int array[] = {16, 11, 9, 10, 5, 6, 8, 1, 2, 4};
     int size = sizeof(array) / sizeof(array[0]);
 
     print_pyramid(array, size);
